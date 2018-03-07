770ff078ac8f2384762864609f8e4820852c701e
Stuart Williams <stuart.williams@bbc.co.uk>
2018-03-07 09:30:14 +0000

    Update Lrud to version 2.1.0 and remove event listeners

--- src/Provider.js
+++ src/Provider.js
@@ -8,44 +8,6 @@ class Provider extends Component {
     }
   }
 
-  _onFocus (node) {
-    if (node.onFocus) {
-      node.onFocus(node)
-    }
-  }
-
-  _onBlur (node) {
-    if (node.onBlur) {
-      node.onBlur(node)
-    }
-  }
-
-  _onMove (node) {
-    if (node.onMove) {
-      node.onMove(node)
-    }
-  }
-
-  _onSelect (node) {
-    if (node.onSelect) {
-      node.onSelect(node)
-    }
-  }
-
-  componentWillMount () {
-    this.props.navigation.on('focus', this._onFocus, this)
-    this.props.navigation.on('blur', this._onBlur, this)
-    this.props.navigation.on('move', this._onMove, this)
-    this.props.navigation.on('select', this._onSelect, this)
-  }
-
-  componentWillUnmount () {
-    this.props.navigation.off('focus', this._onFocus)
-    this.props.navigation.off('blur', this._onBlur)
-    this.props.navigation.off('move', this._onMove)
-    this.props.navigation.off('select', this._onSelect)
-  }
-
   render () {
     return Children.only(this.props.children)
   }
